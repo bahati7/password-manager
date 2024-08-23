1e45a6383885fdd325cd0d7baf65811cc3681761
Bahati Philemon <bahatiphilmon8@gmail.com>
2024-08-23 11:19:32 +0200

    handle the  display of components #4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import logo from "./logo.svg";
+import { useState } from "react";
+import Login from "./components/Login";
+import SendCode from "./components/SendCode";
+
+import {auth} from './firebase'
+import {useAuthState} from 'react-firebase-hooks/auth'
+import ViewCode from "./components/ViewCode";
+
 
 function App() {
+  const [user]=useAuthState(auth)
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleFormSubmit = () => {
+    setIsSubmitted(true); // Set submitted state to true on successful form submission
+  };
+
+  const handleReturnToForm = () => {
+    setIsSubmitted(false); // Reset submitted state
+  };
+  
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="flex justify-center min-h-screen content-center items-center">
+  
+      {user ?isSubmitted ? null : <SendCode onSubmitSuccess={handleFormSubmit} />:<Login/>}  
+   
+        {isSubmitted && <ViewCode onReturnToForm={handleReturnToForm} />}
     </div>
   );
 }
